fb7a54076f9d65c6131533890937448774fc3e6d
1664595314@qq.com <pangaofeng0.0>
2019-07-10 11:23:31 +0800

    初始化

--- src/main/java/com/pan/demo/DemoApplication.java
+++ src/main/java/com/pan/demo/DemoApplication.java
@@ -2,11 +2,8 @@ package com.pan.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletComponentScan;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
 public class DemoApplication {
 
     public static void main(String[] args) {
--- src/test/java/com/pan/demo/发发发.java
+++
@@ -1,8 +0,0 @@
-package com.pan.demo;
-
-/**
- * @author pan
- * @date 2019/7/10 11:13
- */
-public class 发发发 {
-}
